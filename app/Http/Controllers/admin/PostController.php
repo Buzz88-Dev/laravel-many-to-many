696fcdb3406c59b1c1fa4f4b1beb52d5ab202c6c
Buzz88-Dev <omarbuzzoni88@gmail.com>
2022-07-27 09:39:27 +0200

    validation form create

--- app/Http/Controllers/admin/PostController.php
+++ app/Http/Controllers/admin/PostController.php
@@ -5,14 +5,12 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\Tag;
 
 class PostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         $perPage = 20;
@@ -21,67 +19,67 @@ class PostController extends Controller
                                                               // e siccome gli sto passando una sola variabile utilizzo il compact
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function create()
     {
-        return view('admin.posts.create');
+        // mi prendo le categories e le passo al create per stampare una select composta da queste categories
+        // stessa cosa faccio per la collection $tags che verra passato al fieldset Tags in create.blade.php
+        $categories = Category::all();
+        $tags = Tag::all();
+        return view('admin.posts.create', [
+            'categories' => $categories,
+            'tags' => $tags,
+        ]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(Request $request)
     {
-        //
+        // dump($request->all()); // dump: per vedere cosa mi viene passato nel back-end quando clicco save in create.blade.php; decommentare e analizzare
+        // dobbiamo fare la validation e il salvataggio all interno di store()
+        // Validation: lo facciamo direttamente sulla $request
+        $request->validate([
+            'title'     => 'required|string|max:100',
+            'slug'      => 'required|string|max:100|unique:posts',
+            'category_id'  => 'required|integer|exists:categories,id',
+            'tags'      => 'nullable|array',
+            'tags.*'    => 'integer|exists:tags,id',
+            'image'     => 'required_without:content|nullable|url',
+            'content'   => 'required_without:image|nullable|string|max:5000',
+            'excerpt'   => 'nullable|string|max:200',
+        ]);
+
+        // dump($request->all());  // se la validation non passa torniamo nel form; se invece le validation passano ci stampera il dump; dopo aver inserito i campi, salvare e decommentare questo dump()
+        $data = $request->all();
+        // dump($data);
+
+        // salvataggio
+        $post = Post::create($data);
+        $post->tags()->sync($data['tags']);
+
+        // redirect
+        return redirect()->route('admin.posts.show', ['post' => $post->id]);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
+
     public function show(Post $post)
     {
         return view('admin.posts.show', compact('post'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
+
     public function edit(Post $post)
     {
-        //
+        // return view ('admin.posts.edit'), compact('post')
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(Request $request, Post $post)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy(Post $post)
     {
         //
--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,5 +1,105 @@
 @extends('admin.layouts.base')
 
 @section('mainContent')
-    CREATE
+    <h1>Create new post</h1>
+    {{-- quando faccio il submit del form, il  form va a finire in posts.store; metodo store() in PostController --}}
+    <form action="{{ route('admin.posts.store') }}" method="post" novalidate>
+        @csrf
+
+        <div class="mb-3">
+            <label class="form-label" for="title">Title</label>
+            <input class="form-control @error('title') is-invalid @enderror" type="text" name="title" id="title" value="{{ old('title') }}">
+            @error('title')
+            {{-- se c e l errore mi stampa questo messaggio di errore (is-invalid); @error è una specie di if --}}
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+
+        <div class="mb-3">
+            <label class="form-label" for="slug">Slug</label>
+            <input class="form-control @error('slug') is-invalid @enderror" type="text" name="slug" id="slug" value="{{ old('slug') }}">
+            <button type="button" class="btn btn-primary">Reset</button>
+            @error('slug')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+
+        <div class="mb-3">
+            <label class="form-label" for="image">Image</label>
+            <input class="form-control @error('image') is-invalid @enderror" type="url" name="image" id="image" value="{{ old('image') }}">
+            @error('image')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+
+        <div class="mb-3">
+            <label class="form-label" for="category_id">Category</label>
+            {{-- creo le opzioni con tutte le categories che arrivano dal database; guardare metodo create() in PostController --}}
+            <select class="form-select @error('category_id') is-invalid @enderror" name="category_id" id="category_id">
+                <option @if(!old('category_id')) selected @endif disabled value="">Choose...</option>
+                {{-- stampo le categories; dati arrivano dal metodo create() --}}
+                @foreach ($categories as $category)
+                    {{-- analizzare e comprendere @if ($category->id === old('category')) selected @endif; lezione 26/07 minuto 02 04 00 circa --}}
+                    <option value="{{ $category->id }}" @if($category->id == old('category_id')) selected @endif>{{ $category->name }}</option>
+                @endforeach
+            </select>
+            @error('category_id')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+
+        <fieldset class="mb-3">
+            <legend>Tags</legend>
+            @foreach ($tags as $tag)
+                <div class="form-check">
+                    {{-- ricordarsi di aggiungere [] al name per avere un array come valore di ritorno --}}
+                    <input
+                        class="form-check-input"
+                        type="checkbox"
+                        name="tags[]"
+                        value="{{ $tag->id }}"
+                        id="tag-{{ $tag->id }}"
+                        @if(in_array($tag->id, old('tags') ?: [])) checked @endif
+                    >
+                    <label class="form-check-label" for="tag-{{ $tag->id }}">{{ $tag->name }}</label>
+                </div>
+            @endforeach
+
+            @error('tags')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </fieldset>
+
+        <div class="mb-3">
+            <label class="form-label" for="content">Content</label>
+            <textarea class="form-control @error('content') is-invalid @enderror" name="content" id="content">{{ old('content') }}</textarea>
+            @error('content')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+
+        <div class="mb-3">
+            <label class="form-label" for="excerpt">Excerpt</label>
+            <textarea class="form-control @error('excerpt') is-invalid @enderror" name="excerpt" id="excerpt">{{ old('excerpt') }}</textarea>
+            @error('excerpt')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+
+        <button type="submit" class="btn btn-primary">Save</button>
+    </form>
 @endsection
